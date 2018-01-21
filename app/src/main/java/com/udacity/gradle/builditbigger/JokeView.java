7442adc098f2b79ac93c3c15237b0a8b4e8bd7ad
LBR2048 <leo.ardjomand@gmail.com>
2018-01-21 16:25:20 -0200

    Implement GCE that serves the jokes to the app

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/JokeView.java
@@ -0,0 +1,9 @@
+package com.udacity.gradle.builditbigger;
+
+/**
+ * Created by unity on 21/01/18.
+ */
+
+public interface JokeView {
+    void showJoke(String joke);
+}
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,8 +1,11 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.util.Pair;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,7 +15,7 @@ import com.example.jokeprovider.JokeProvider;
 import com.example.jokeviewer.JokeViewerActivity;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements JokeView {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,15 +47,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        // Get joke from Java library
-        JokeProvider jokeProvider = new JokeProvider();
-        String joke = jokeProvider.getJoke();
+        // Get joke from GCE server
+        new GetJokeAsyncTask().execute(this);
+    }
 
+    @Override
+    public void showJoke(String joke) {
         // Send joke to another Activity inside Android library
         Intent intent = new Intent(this, JokeViewerActivity.class);
         intent.putExtra(JokeViewerActivity.EXTRA_JOKE, joke);
         startActivity(intent);
     }
-
-
 }
