c2535e05b1a0909f7a338998f296483f5c9ffdfd
Leonardo Ardjomand <leonardo.ardjomand@fluig.com>
2018-01-31 17:58:26 -0200

    Call GCE through its API

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -49,6 +49,7 @@ public class MainActivity extends AppCompatActivity implements JokeView {
     public void tellJoke(View view) {
         // Get joke from GCE server
         new GetJokeAsyncTask().execute(this);
+//        new GetJokeAsyncTask().execute("Nome");
     }
 
     @Override
