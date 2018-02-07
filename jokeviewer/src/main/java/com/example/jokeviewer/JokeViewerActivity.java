b9db109a5d215825a0c016d910edd5333310f0f2
Leonardo Ardjomand <leonardo.ardjomand@fluig.com>
2018-02-07 10:40:45 -0200

    Check if expected intent extra was received

--- jokeviewer/src/main/java/com/example/jokeviewer/JokeViewerActivity.java
+++ jokeviewer/src/main/java/com/example/jokeviewer/JokeViewerActivity.java
@@ -2,11 +2,13 @@ package com.example.jokeviewer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class JokeViewerActivity extends AppCompatActivity {
 
+    public static final String LOG_TAG = JokeViewerActivity.class.getSimpleName();
     public static final String EXTRA_JOKE = "extra_joke";
 
     @Override
@@ -16,7 +18,11 @@ public class JokeViewerActivity extends AppCompatActivity {
 
         TextView jokeTextView = findViewById(R.id.jokeTextView);
 
-        String joke = getIntent().getStringExtra(EXTRA_JOKE);
-        jokeTextView.setText(joke);
+        if (getIntent() != null && getIntent().hasExtra(EXTRA_JOKE)) {
+            String joke = getIntent().getStringExtra(EXTRA_JOKE);
+            jokeTextView.setText(joke);
+        } else {
+            Log.w(LOG_TAG, EXTRA_JOKE + " intent extra not received");
+        }
     }
 }
